f795e244c36ad764572e24e269bc9a00bf4e342d
MoJo <msjche@gmail.com>
2017-10-22 23:11:35 -0700

    add the stuff

---
+++ autostart.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+run "megasync"
+run "xscreensaver --no-splash"
+run "/usr/bin/redshift"
+run "/usr/bin/dropbox"
+run "insync start"
+run "compton"
+run "/usr/bin/redshift"
+run "urxvtd -q -f -o"
+run "mpd"
+run "nm-applet"
+pkill conky
+
+sleep 3
+run "conky -c /home/msjche/.config/awesome/conky/conkyrc"
